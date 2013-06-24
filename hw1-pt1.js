7bc8d4674760f78ba8dd56bd687b85c482bcd703
Bruno Faria <nunes@outlook.com>
2013-06-24 10:50:48 -0300

    initial commit

---
+++ hw1-pt1.js
@@ -0,0 +1,13 @@
+/*
+* @Author: Bruno Faria
+* Descript: Homework1 Part1 - Startup Engineering
+* 			Write to file using node.js	
+*/
+
+var fs = require('fs');
+var outfile = "hello.txt";
+var out = "A startup is a business built to grow rapidly.\n";
+
+// writes @out to @outfile
+fs.writeFileSync(outfile, out);  
+console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
---
+++ hw1-pt2.js
@@ -0,0 +1,49 @@
+/*
+* @Author: Bruno Faria
+* Descript: Homework1 Part2 - Startup Engineering
+* 			Prime Numbers	
+*/
+
+var fs = require('fs');
+
+// test if a number is prime
+var isPrime = function(n) 
+{
+	var prime = false;
+	// base prime number
+	if (n == 2) {
+		prime = 1;
+	}
+	// for all the others
+	for (var i=2; i < n; i++) {
+		if (n % i == 0) {
+			prime = 0;
+			break;
+		}
+		else {
+			prime = 1;
+		}
+	}
+	return prime;
+}
+
+// outputs the first @max primes to primes.txt file
+var hw = function (max) {
+	var countPrimes = 0;
+	var primeArray = [];
+	var outfile = 'primes.txt';
+	for (var i = 2; countPrimes < max; i++) {
+		var prime = isPrime(i); 
+		if(prime == 1) {
+			primeArray.push(i);
+			countPrimes++;
+		}
+	}
+	// writes the array into file
+	fs.writeFileSync(outfile, primeArray);  
+	console.log("Script: " + __filename + "\nWrote: " + primeArray + "\nTo: " + outfile);
+}
+
+// gets first 100 prime numbers
+hw(100);
+
